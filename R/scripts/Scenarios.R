d396dbc02e7e483a029dd5c63f5ea2ba263ca846
Sofie vd Brand <>
2021-02-09 16:58:21 +0100

    Simulate data 1 run s1
    Data generation function + add to do list to Readme

--- R/scripts/Scenarios.R
+++ R/scripts/Scenarios.R
@@ -4,15 +4,19 @@
 ### Define scenario's
 
 #### To do:
-## Add smart solution to define sample sizes!
+## Think of solutions to add sample size and beta coefficient more efficiently
 
 ### Setting up ###
 library(tidyverse)
 library(pmsampsize)
 set.seed(123)
 
-############### Scenario 1 #################
-## Varying data gen mechanism
+
+########################
+###### Scenario 1 ######
+########################
+
+## Data gen mechanism
 AUC1 <- 0.75
 dim1 <- 10
 n1 <- as.factor(c("at", "below"))
@@ -22,68 +26,35 @@ prev1 <- c(0.05, 0.2)
 models1 <-  as.factor(c("OLS",  "Firth", "SVM"))
 
 ## all combinations:
-s1 <- expand.grid(AUC = AUC1, dim = dim1, n_state = n1, prev = prev1, model = models1)
-
-############### Scenario 2 #################
-## Varying data gen mechanism
-AUC2 <- 0.75
-dim2 <- c(6, 32, 60)
-n2 <- as.factor(c("at", "below"))
-prev2 <- 0.2
-
-## Models used:
-models2 <-  as.factor(c("OLS",  "Firth", "SVM"))
-
-## all combinations:
-s2 <- expand.grid(AUC = AUC2, prev = prev2, dim = dim2, n_state = n2, model = models2)
+s1 <- expand.grid(AUC = AUC1, dim = dim1, n_state = n1, prev = prev1, model = models1, KEEP.OUT.ATTRS = F)
 
 
-############### Scenario 3 ##################
-## Varying data gen mechanism
-AUC3 <- 0.75
-dim3 <- 10
-n3 <- as.factor(c("at", "below"))
-prev3 <- 0.2
-
-## Models combinations:
-models3 <-  as.factor(c("OLS", "Ridge", "Firth", "LASSO", "Elastic Net", "SVM", "ANN", "RF"))
-
-## all combinations:
-s3 <- expand.grid(AUC = AUC3, prev = prev3, dim = dim3, n_state = n3, model = models3)
-
-#### Expected R^2 ####
+##################
+## Expected R^2 ##
+##################
 
 source("scripts/Data generation functions.R")
-# Within the script above, a desired level of R^2 Cs is approximated for a given
-# prevalence and C-statistic. The same script also allows for the calculation of
-# the minimum sample size in each simulation.
-
-# # If more changes:
-# AUC <- c(0.6, 0.75, 0.9)
-# prevalence <- c(0.05, 0.2)
-# expand.grid(AUC, prevalence)
+# Within the script above, the function "approximate_R2" is called. Here, a desired level of R^2 Cs is approximated for a given
+# prevalence and C-statistic. This can then be used to optimize beta coefficients and to determine the minimal sample size needed. 
 
 ## Determine which R2 belongs to each situation:
-R2 <- c("prev_0.05" = approximate_R2(auc = AUC1, prev = 0.05)$R2.coxsnell,
-        "prev_0.2" = approximate_R2(auc = AUC1, prev = 0.2)$R2.coxsnell)
+R2 <- c(approximate_R2(auc = AUC1, prev = prev1[1])$R2.coxsnell,
+        approximate_R2(auc = AUC1, prev = prev1[2])$R2.coxsnell)
 
-s1 <- s1 %>% mutate(R2 = case_when(prev == prev1[1] ~ R2[1], prev == prev1[2] ~ R2[2]))
-s2$R2 <- R2[2]
-s3$R2 <- R2[2]
+s1 <- s1 %>% mutate(R2 = case_when(prev == prev1[1] ~ R2[1], 
+                                   prev == prev1[2] ~ R2[2]))
 
+########################
+## Actual sample size ##
+########################
 
-#### Actual sample size ####
 at_n_10x_e.2 <- pmsampsize(type = "b", parameters = 10, prevalence = 0.2, rsquared = R2[2])$sample_size
 at_n_10x_e.05 <- pmsampsize(type = "b", parameters = 10, prevalence = 0.05, rsquared = R2[1])$sample_size
-# at_n_32x_e.2 <- pmsampsize(type = "b", parameters = 33, prevalence = 0.2, rsquared = R2[2])$sample_size
-# at_n_60x_e.2 <- pmsampsize(type = "b", parameters = 60, prevalence = 0.2, rsquared = R2[2])$sample_size
-# 
+
 below_n_10x_e.2 <- ceiling(0.8 * at_n_10x_e.2)
 below_n_10x_e.05<- ceiling(0.8 * at_n_10x_e.05)
 # below_n_32x_e.2<- ceiling(0.8 * at_n_32x_e.2)
 # below_n_60x_e.2<- ceiling(0.8 * at_n_60x_e.2)
-
-#
 
 s1 <- s1 %>%
   mutate(
@@ -96,6 +67,101 @@ s1 <- s1 %>%
       )
     )
 
+##################
+## coefficients ##
+##################
+
+beta_0.05 <- readRDS("Data/Simulation settings/Scenario 1/Betas/Betas_prev_0.05_halfstrong.Rds")
+beta_0.2 <- readRDS("Data/Simulation settings/Scenario 1/Betas/Betas_prev_0.2_halfstrong.Rds")
+
+s1 <- s1 %>% 
+  mutate(par1 = case_when(prev == 0.05 ~ beta_0.05[1],
+                          prev == 0.2 ~ beta_0.2[1],
+                          TRUE ~ NA_real_
+  )) %>%
+  mutate(par2 = case_when(prev == 0.05 ~ beta_0.05[2],
+                          prev == 0.2 ~ beta_0.2[2],
+                          TRUE ~ NA_real_))
+
+
+
+#######################################################################################################################
+#######################################################################################################################
+############################################### WORK IN PROGRESS ######################################################
+#######################################################################################################################
+#######################################################################################################################
+
+########################
+###### Scenario 2 ######
+########################
+
+## Data gen mechanism
+AUC2 <- 0.75
+dim2 <- c(6, 32, 60)
+n2 <- as.factor(c("at", "below"))
+prev2 <- 0.2
+
+## Models used:
+models2 <-  as.factor(c("OLS",  "Firth", "SVM"))
+
+## all combinations:
+s2 <- expand.grid(AUC = AUC2, prev = prev2, dim = dim2, n_state = n2, model = models2, KEEP.OUT.ATTRS = F)
+
+#################
+## Expected R2 ##
+#################
+
+s2$R2 <- R2[2]
+
+########################
+## Actual sample size ##
+########################
+
+# at_n_10x_e.2 <- pmsampsize(type = "b", parameters = 10, prevalence = 0.2, rsquared = R2[2])$sample_size
+# at_n_32x_e.2 <- pmsampsize(type = "b", parameters = 33, prevalence = 0.2, rsquared = R2[2])$sample_size
+# at_n_60x_e.2 <- pmsampsize(type = "b", parameters = 60, prevalence = 0.2, rsquared = R2[2])$sample_size
+# below_n_10x_e.2 <- ceiling(0.8 * at_n_10x_e.2)
+# below_n_32x_e.2 <- ceiling(0.8 * at_n_32x_e.2)
+# below_n_60x_e.2 <- ceiling(0.8 * at_n_60x_e.2)
+
+
+########################
+###### Scenario 3 ######
+########################
+
+## Data gen mechanism
+AUC3 <- 0.75
+dim3 <- 10
+n3 <- as.factor(c("at", "below"))
+prev3 <- 0.2
+
+## Models combinations:
+models3 <-  as.factor(c("OLS", "Ridge", "Firth", "LASSO", "Elastic Net", "SVM", "ANN", "RF"))
+
+## all combinations:
+s3 <- expand.grid(AUC = AUC3, prev = prev3, dim = dim3, n_state = n3, model = models3, KEEP.OUT.ATTRS = F)
+
+#################
+## Expected R2 ##
+#################
+
+s3$R2 <- R2[2]
+
+
+########################
+## Actual sample size ##
+########################
+
+# at_n_10x_e.2 <- pmsampsize(type = "b", parameters = 10, prevalence = 0.2, rsquared = R2[2])$sample_size
+# below_n_10x_e.2 <- ceiling(0.8 * at_n_10x_e.2)
+
+
+
+
+
+###################################
+## Rounding the script to an end ##
+###################################
 
 # Remove everything except the three scenario matrices
 rm(list=ls()[! ls() %in% c("s1","s2", "s3", "R2")])
@@ -104,3 +170,16 @@ rm(list=ls()[! ls() %in% c("s1","s2", "s3", "R2")])
 ####################################
 ####################################
 ## END SCRIPT
+
+
+# # If more changes, for example:
+# AUC <- c(0.6, 0.75, 0.9)
+# prevalence <- c(0.05, 0.2)
+# test <-  expand.grid(AUC = AUC, prevalence = prevalence)
+# 
+# test$R2 <- 0
+# for (i in 1:nrow(test)){
+#   test[i,3] <- approximate_R2(auc = test[i,1], prev = test[i,2])$R2.coxsnell
+# }
+
+
--- README.md
+++ README.md
@@ -5,3 +5,24 @@
 
 The initial research proposal and a report based on the work done during the first half of the thesis can be found in their respective folders.
 In the R folder the code to run simulations can be found.
+
+
+### To do list:
+
+**Within scenario definitions**
+Concerning the `Scenarios.R` script:
+[ ] Come up with solution, maybe a function or something, to get sample size and beta coefficients into scenario matrix
+[ ] Save scenario matrices once they have been finalized
+
+
+**Within Data generation**
+Concerning the `Data generation scenario 1.R`:
+[ ] Save the generated data in a folder per sub-scenario
+
+Concerning the `Data generation functions.R`:
+[ ] Create a test script for beta optimization
+[ ] Add options for different predictor effects in beta optimization
+[ ] Add options for different number of predictors within beta optimization
+[ ] Add options to sample from different distributions, other than multivariate normal
+[ ] Check convergence of optimization, especially when changing effect and number of predictors
+[ ] Add a check for how many events have been sampled in `generate_data` function
