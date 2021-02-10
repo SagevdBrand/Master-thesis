56161f69644d10d759e8737ded9df6f02d64478f
Sofie vd Brand <>
2021-02-10 12:26:03 +0100

    Saving first run of results
    Still thinking about whether it would be useful to write the results in these folders, instead of the data. Maybe it's more useful to keep the data as one list, so lapply() can be used for the rest

--- R/scripts/Data generation scenario 1.R
+++ R/scripts/Data generation scenario 1.R
@@ -15,3 +15,9 @@ source("scripts/Data generation functions.R")
 system.time(s1_data <- generate_data(s1))
 
 lapply(lapply(s1_data,'[[', 11), mean) # check whether the prevalence is somewhat okay
+
+for(i in 1:length(s1_data)) {
+  s1_path <- paste0("Data/simulation data/scenario 1/", i) # create right path
+  saveRDS(assign(paste0("s1_", i), s1_data[[i]]), file = paste0(s1_path,"/s1_", i)) # add name of file to path
+}
+
