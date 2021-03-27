025b8af254158bf67082995e1b6e63148abbbe04
Sofie vd Brand <sofievandenbrand@live.nl>
2021-03-27 22:37:49 +0100

    add sh files for running

---
+++ src/setup_packages.R
@@ -0,0 +1,18 @@
+###################################
+###################################
+###################################
+## Libraries
+
+install.packages("tidyverse")
+install.packages("pmsampsize")
+install.packages("MASS")
+install.packages("rms")
+install.packages("glue")
+install.packages("logistf")
+install.packages("glmnet")
+install.packages("glmnetUtils")
+install.packages("e1071")
+install.packages("caret")
+install.packages("nnet")
+install.packages("rpart")
+install.packages("randomForest")
