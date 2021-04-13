a047bddb571d7d3a738e22513bdc511f8d9cced2
Sofie vd Brand <sofievandenbrand@live.nl>
2021-04-13 09:45:49 +0200

    Add supplementary data and remove bash scripts

--- run_many.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sbatch --array=1-2 run.sh
--- vars.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-R_LIBS=/home/julius_bs/svandenbrand/r_libs
-export R_LIBS
-PATH=/hpc/local/CentOS7/julius_bs/R-4.0.2/R-4.0.2/bin/:$PATH
-export PATH
